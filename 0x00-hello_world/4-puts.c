a677dafa6ed9ad2adb2619e9abb8e1b4760936d1
steven SHYAKA <shyastyve@gmail.com>
2023-02-16 08:15:22 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -A programm that prints a line with function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
