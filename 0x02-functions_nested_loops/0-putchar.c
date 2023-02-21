bf9dee0146a69bc4d6f6736bdc0813ed69b3fb57
steven SHYAKA <shyastyve@gmail.com>
2023-02-21 06:22:16 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-        _putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -16,6 +16,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
