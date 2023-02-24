66d973e1a0ab21f140501f467d244b3fc25c9f3d
steven SHYAKA <shyastyve@gmail.com>
2023-02-24 11:39:48 -0800

    prime factor

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: takes in a integer for size of triangle
+ */
+
+void print_triangle(int size)
+
+{
+
+	int row, column, k;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+
+	for (row = 0; row < size; row++)
+	{
+
+		for (column = size - row; column > 1; column--)
+		{
+			putchar(' ');
+		}
+
+		for (k = row + column; k >= 1; k--)
+		{
+
+			putchar('#');
+		}
+
+		putchar('\n');
+	}
+}
